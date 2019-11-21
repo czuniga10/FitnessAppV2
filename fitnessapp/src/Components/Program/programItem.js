834c63a8051716bb698ad12efbae7e2155496051
Chad Zuniga <chad.zuniga10@gmail.com>
2019-11-20 22:12:14 -0700

    removed bad images

--- fitnessapp/src/Components/Program/programItem.js
+++ fitnessapp/src/Components/Program/programItem.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 
-
-
 class ProgramItem extends Component {
     render() {
+        let test = Math.floor(Math.random()*8) +1
         const {id, name } = this.props;
     return(
         <div className="program-wrapper">
             {name}
+            <img src={require('../../Assets/'+test+'.png')} width="50" height="50" alt='img'/>
         </div>
     )}
 }
