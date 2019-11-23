4a99c80f4663da9ad56c053a913b0267dacbd327
Chad Zuniga <chad.zuniga10@gmail.com>
2019-11-22 21:31:58 -0700

    removed image in program card for now

--- fitnessapp/src/Components/Program/programItem.js
+++ fitnessapp/src/Components/Program/programItem.js
@@ -2,12 +2,10 @@ import React, {Component} from 'react';
 
 class ProgramItem extends Component {
     render() {
-        let test = Math.floor(Math.random()*2) +1
         const {id, name } = this.props;
     return(
         <div className="program-wrapper">
             {name}
-            <img src={require('../../Assets/'+test+'.png')} width="50" height="50" alt='img'/>
         </div>
     )}
 }
