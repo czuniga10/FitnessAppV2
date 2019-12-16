5ebcaf34c0fea04748bcb4a0f1bb885a08d0eb91
Chad Zuniga <chad.zuniga10@gmail.com>
2019-12-15 21:29:12 -0700

    updated store to include updateChecked reducer

--- fitnessapp/src/store.js
+++ fitnessapp/src/store.js
@@ -1,8 +1,12 @@
 import {createStore,combineReducers} from 'redux';
 import userInfo from './Reducers/user.reducer';
+import updateChecked from './Reducers/checked.reducer';
+
+
 
 let rootReducer = combineReducers({
-    userInfo
+    userInfo,
+    updateChecked
 })
 
 export default createStore( rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
