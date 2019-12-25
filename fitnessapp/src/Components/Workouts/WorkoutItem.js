9227b491b650564738692521e1cf0a9a93273db8
Chad Zuniga <chad.zuniga10@gmail.com>
2019-12-24 21:30:45 -0700

    strikethrough now occurs when check is checked

--- fitnessapp/src/Components/Workouts/WorkoutItem.js
+++ fitnessapp/src/Components/Workouts/WorkoutItem.js
@@ -9,7 +9,7 @@ class WorkoutItem extends Component {
         super(props)
         this.state = {
             isChecked: '',
-            isStrikeThrough: false
+            done: false
         }
         this.handleChecked = this.handleChecked.bind(this);
     }
@@ -23,22 +23,25 @@ class WorkoutItem extends Component {
             .then(res => {
                 this.setState({
                     isChecked: res.data[0].iscomplete,
-                    isStrikeThrough: !this.state.isStrikeThrough
+                    done: !this.state.done
                 })
                this.props.updateCheckedRedux(res.data[0].iscomplete)
             })
     }
 
     render() {
+        let style = {
+            textDecoration: this.state.done ? "red line-through" : "none",
+          };
         console.log(this.state)
         const {id, index, name, sets, reps, iscomplete } = this.props;
 
     return(
         <div className="item-wrapper">
             {/* <div className="item name"> {id}</div>  */}
-           <div className="item name" > {name}</div> 
-           <div className="item sets"> {sets}</div>
-           <div className="item reps"> {reps}</div>         
+           <div className="item name" style={style}>  {name}</div> 
+           <div className="item sets" style={style}> {sets}</div>
+           <div className="item reps" style={style}> {reps}</div>         
            <input className="complete" type="checkbox" onChange={() => {this.handleChecked(id)}}/>
         </div>
     )}
