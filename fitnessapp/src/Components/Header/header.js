6cfc0b516ce859a1c0e302fd75242b6f4e1f22de
Chad Zuniga <chad.zuniga10@gmail.com>
2020-01-11 23:29:40 -0700

    added new div for menu holder

--- fitnessapp/src/Components/Header/header.js
+++ fitnessapp/src/Components/Header/header.js
@@ -27,17 +27,22 @@ class Header extends Component {
         // - add new section to header for drop down menu
         // - add isMenuActive state to the class of the new drop down menu so it only appears when true
         // - Grey out rest of page when it is active
-        <div className="header-wrapper">
-            <div className="title">
-                <img className="image" src={logo} />
-            </div>
-            <button>
-                <div className="menu-wrapper" onClick={() => {this.handleMenu()}}>
-                    <div className={`bar top ${isMenuActive ? "topOpen" : ""}`}></div>
-                    <div className={`bar middle ${isMenuActive ? "middleOpen" : ""}`}></div>
-                    <div className={`bar bottom ${isMenuActive ? "bottomOpen" : ""}`}></div>
+        <div className="header">
+            <div className="header-wrapper">
+                <div className="title">
+                    <img className="image" src={logo} />
                 </div>
-            </button>
+                <button>
+                    <div className="menu-wrapper" onClick={() => {this.handleMenu()}}>
+                        <div className={`bar top ${isMenuActive ? "topOpen" : ""}`}></div>
+                        <div className={`bar middle ${isMenuActive ? "middleOpen" : ""}`}></div>
+                        <div className={`bar bottom ${isMenuActive ? "bottomOpen" : ""}`}></div>
+                    </div>
+                </button>
+            </div>
+            <div className="menu-wrapper">
+                
+            </div>
         </div>
     )}
 }
